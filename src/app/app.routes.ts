5fc4336cbfeb6fd92f652b7d5c92c79d2af479cb
Jakub Senderowski <jakubsenderowskica@gmail.com>
2026-01-09 14:40:36 +0100

    refactor: refactor project with core/shared/features architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
+import { DASHBOARD_ROUTES } from './features/dashboard/dashboard.routes';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
+	{ path: '', pathMatch: 'full', redirectTo: '/dashboard' },
+	{ path: 'dashboard', loadChildren: () => DASHBOARD_ROUTES },
 ];
