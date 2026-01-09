a509651f5920a116e96e2366060fbb6a706ecadd
Jakub Senderowski <jakubsenderowskica@gmail.com>
2026-01-09 15:38:37 +0100

    feat: add Merchant interface model

--- src/app/features/merchants/merchants.component.ts
+++ src/app/features/merchants/merchants.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-merchants',
-  imports: [],
-  templateUrl: './merchants.component.html',
-  styleUrl: './merchants.component.scss'
+	selector: 'app-merchants',
+	imports: [],
+	templateUrl: './merchants.component.html',
+	styleUrl: './merchants.component.scss',
 })
-export class MerchantsComponent {
+export class MerchantsComponent {}
 
+export interface Merchant {
+	id: string;
+	createdAt: Date;
+	transactionLimit: number;
+	paymentMethods: string[];
+	companyName: string;
+	status: 'active' | 'inactive' | 'blocked';
+	nip: string;
+	companyLocation: string;
+	address: string;
+	email: string;
+	companyType: string;
 }
