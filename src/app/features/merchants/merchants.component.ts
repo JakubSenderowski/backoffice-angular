87563c10cfb51c5b8eba1b9935e7d0178ef83e6e
Jakub Senderowski <jakubsenderowskica@gmail.com>
2026-01-11 12:34:43 +0100

    feat: add status color badges to merchants table

--- src/app/features/merchants/merchants.component.ts
+++ src/app/features/merchants/merchants.component.ts
@@ -4,9 +4,10 @@ import { Merchant } from '../../core/models/merchant.interface';
 import { OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTagModule } from 'ng-zorro-antd/tag';
 @Component({
 	selector: 'app-merchants',
-	imports: [CommonModule, NzTableModule],
+	imports: [CommonModule, NzTableModule, NzTagModule],
 	templateUrl: './merchants.component.html',
 	styleUrl: './merchants.component.scss',
 })
@@ -17,5 +18,18 @@ export class MerchantsComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.merchantService.getMerchants().subscribe((data) => (this.merchants = data));
+	}
+
+	getStatusColor(status: string): string {
+		switch (status) {
+			case 'active':
+				return 'green';
+			case 'inactive':
+				return 'orange';
+			case 'blocked':
+				return 'red';
+			default:
+				return 'default';
+		}
 	}
 }
