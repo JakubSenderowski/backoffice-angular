435ba148cdd8dc16085934b7d42f7d5b303ab331
Jakub Senderowski <jakubsenderowskica@gmail.com>
2026-01-11 13:34:11 +0100

    feat: add auth guard to protect routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { MONITORING_ROUTES } from './features/monitoring/monitoring.routes';
 import { REPORTS_ROUTES } from './features/reports/reports.routes';
 import { SETTLEMENTS_ROUTES } from './features/settlement/settlement.routes';
 import { AUTH_ROUTES } from './features/auth/auth.routes';
+import { authGuard } from './core/guards/auth.guard';
 export const routes: Routes = [
 	{ path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-	{ path: 'dashboard', loadChildren: () => DASHBOARD_ROUTES },
-	{ path: 'merchants', loadChildren: () => MERCHANT_ROUTES },
-	{ path: 'monitoring', loadChildren: () => MONITORING_ROUTES },
-	{ path: 'reports', loadChildren: () => REPORTS_ROUTES },
-	{ path: 'settlement', loadChildren: () => SETTLEMENTS_ROUTES },
+	{ path: 'dashboard', loadChildren: () => DASHBOARD_ROUTES, canActivate: [authGuard] },
+	{ path: 'merchants', loadChildren: () => MERCHANT_ROUTES, canActivate: [authGuard] },
+	{ path: 'monitoring', loadChildren: () => MONITORING_ROUTES, canActivate: [authGuard] },
+	{ path: 'reports', loadChildren: () => REPORTS_ROUTES, canActivate: [authGuard] },
+	{ path: 'settlement', loadChildren: () => SETTLEMENTS_ROUTES, canActivate: [authGuard] },
 	{ path: 'auth', loadChildren: () => AUTH_ROUTES },
 ];
