6222aa29cd199560af25b4d239fb4286aa94baad
Jakub Senderowski <jakubsenderowskica@gmail.com>
2026-01-11 12:25:12 +0100

    feat: display merchants data in table

--- src/app/features/merchants/merchants.component.ts
+++ src/app/features/merchants/merchants.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
-
+import { MerchantService } from '../../core/services/merchant.service';
+import { Merchant } from '../../core/models/merchant.interface';
+import { OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NzTableModule } from 'ng-zorro-antd/table';
 @Component({
 	selector: 'app-merchants',
-	imports: [],
+	imports: [CommonModule, NzTableModule],
 	templateUrl: './merchants.component.html',
 	styleUrl: './merchants.component.scss',
 })
-export class MerchantsComponent {}
+export class MerchantsComponent implements OnInit {
+	merchants: Merchant[] = [];
+
+	constructor(private merchantService: MerchantService) {}
+
+	ngOnInit(): void {
+		this.merchantService.getMerchants().subscribe((data) => (this.merchants = data));
+	}
+}
