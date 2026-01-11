431e1b9026d013a579edc151ffc009753d5a0c50
Jakub Senderowski <jakubsenderowskica@gmail.com>
2026-01-11 13:04:12 +0100

    feat: add live search filter to merchants table

--- src/app/features/merchants/merchants.component.ts
+++ src/app/features/merchants/merchants.component.ts
@@ -6,20 +6,27 @@ import { CommonModule } from '@angular/common';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzTagModule } from 'ng-zorro-antd/tag';
 import { NzPaginationModule } from 'ng-zorro-antd/pagination';
+import { FormsModule } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 @Component({
 	selector: 'app-merchants',
-	imports: [CommonModule, NzTableModule, NzTagModule, NzPaginationModule],
+	imports: [CommonModule, NzTableModule, NzTagModule, NzPaginationModule, NzInputModule, FormsModule, NzIconModule],
 	templateUrl: './merchants.component.html',
 	styleUrl: './merchants.component.scss',
 })
 export class MerchantsComponent implements OnInit {
 	merchants: Merchant[] = [];
-
+	searchValue: string = '';
+	filteredMerchants: Merchant[] = [];
 	constructor(private merchantService: MerchantService) {}
 
 	ngOnInit(): void {
-		this.merchantService.getMerchants().subscribe((data) => (this.merchants = data));
-		console.log('Merchants loaded:', this.merchants);
+		this.merchantService.getMerchants().subscribe((data) => {
+			this.merchants = data;
+			this.filteredMerchants = data;
+			console.log('Merchants loaded:', data);
+		});
 	}
 
 	getStatusColor(status: string): string {
@@ -39,4 +46,15 @@ export class MerchantsComponent implements OnInit {
 	sortByLimit = (a: Merchant, b: Merchant) => a.transactionLimit - b.transactionLimit;
 	sortByStatus = (a: Merchant, b: Merchant) => a.status.localeCompare(b.status);
 	sortByLocation = (a: Merchant, b: Merchant) => a.companyLocation.localeCompare(b.companyLocation);
+
+	onSearch(): void {
+		const value = this.searchValue.toLocaleLowerCase();
+		if (!value) {
+			this.filteredMerchants = this.merchants;
+			return;
+		}
+		this.filteredMerchants = this.merchants.filter((merchant) =>
+			merchant.companyName.toLocaleLowerCase().includes(value),
+		);
+	}
 }
